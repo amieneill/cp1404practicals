78d1ac18915545f1c78258992f33ee8fb46d7f20
amie <amie.neill@my.jcu.edu.au>
2025-06-01 16:19:58 +1000

    Add initial menu loop.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -7,4 +7,21 @@ Name: Amie Neill
 MENU = "Menu:\n(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
 print(MENU)
 
+choice = input("Choice: ").upper()
+while choice != 'Q':
+    if choice == 'G':
+        pass
+    elif choice == 'P':
+        pass
+    elif choice == 'S':
+        pass
+    else:
+        print("Invalid choice")
+        print(MENU)
+        choice = input("Choice: ").upper()
+print("Farewell")
+
+
+
+
 
