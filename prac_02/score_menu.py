b6cd2005accb6e528039615b1cb2c6e81c74dbe5
amie <amie.neill@my.jcu.edu.au>
2025-06-01 17:15:20 +1000

    Refactor score menu program to use functions.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,24 +4,50 @@ Scores Menu Program
 Name: Amie Neill
 """
 
-MENU = "Menu:\n(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
-print(MENU)
 
-choice = input("Choice: ").upper()
-while choice != 'Q':
-    if choice == 'G':
-        pass
-    elif choice == 'P':
-        pass
-    elif choice == 'S':
-        pass
-    else:
-        print("Invalid choice")
-        print(MENU)
+def main():
+    """Run score menu program."""
+    score = []
+    menu = "Menu:\n(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
+    print(menu)
+    choice = input("Choice: ").upper()
+    while choice != 'Q':
+        if choice == 'G':
+            score = get_valid_score()
+        elif choice == 'P':
+            determine_score_result(score)
+        elif choice == 'S':
+            print_asterisks(score)
+        else:
+            print("Invalid choice")
+        print(menu)
         choice = input("Choice: ").upper()
-print("Farewell")
+    print("Farewell")
 
 
+def get_valid_score():
+    """Get valid score from user."""
+    score = int(input("Score: "))
+    while score < 0 or score > 100:
+        print("Invalid score.")
+        score = int(input("Score: "))
+    return score
 
 
+def determine_score_result(score):
+    """Determine score result."""
+    if score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    print(f"Based on your score of {score}, your result is considered {result}.")
 
+
+def print_asterisks(score):
+    """Print asterisks for score."""
+    print('*' * score)
+
+
+main()
