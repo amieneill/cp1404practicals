919aa8448ade2cf75a91fa49f35154b271e50c68
amie <amie.neill@my.jcu.edu.au>
2025-06-01 15:08:31 +1000

    Refactor password check program to use functions.

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,10 +4,26 @@ Password Check Program
 Name: Amie Neill
 """
 
-minimum_password_length = 10
-password = input("Password: ")
-while len(password) < minimum_password_length:
-    print("Password too short")
-    password = input("Password: ")
 
-print('*' * len(password))
+def main():
+    """Run password check program."""
+    minimum_password_length = 10
+    password = get_password(minimum_password_length)
+    print_asterisks(password)
+
+
+def get_password(minimum_password_length):
+    """Get password and validate length."""
+    password = input("Password: ")
+    while len(password) < minimum_password_length:
+        print("Password too short")
+        password = input("Password: ")
+    return password
+
+
+def print_asterisks(password):
+    """Print asterisks based on password length."""
+    print('*' * len(password))
+
+
+main()
