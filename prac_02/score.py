e3eaa400441c795a23b0fdac7619ad0f8ae40b98
amie <amie.neill@my.jcu.edu.au>
2025-06-01 15:32:33 +1000

    Refactor scores program to use functions.

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,14 +4,25 @@ Scores Program
 Name: Amie Neill
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
-    score = float(input("Enter score: "))
 
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Run scores program."""
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    result = determine_score_result(score)
+    print(result)
+
+
+def determine_score_result(score):
+    """Determine score based on score input."""
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
