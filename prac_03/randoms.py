958e422bc88a882b85beb6f73163c12aa33239a5
amie <amie.neill@my.jcu.edu.au>
2025-06-08 12:52:04 +1000

    Complete answers to the questions in comments for line 2

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -11,6 +11,16 @@ print(random.randint(5, 20))  # line 1
 # Smallest number: 5
 # Largest number: 20
 
+print(random.randrange(3, 10, 2))  # line 2
+# Output is a random number between 3 and 9, with a step of 2
+# Smallest number: 3
+# Largest number: 9
+# No, line 2 would not be able to produce a 4
+# In this case it'll only produce an odd number in range
+
+
+
+
 
 
 
