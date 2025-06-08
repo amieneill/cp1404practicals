848ca4d5a25a0141b91ea54e43428c88870f5beb
amie <amie.neill@my.jcu.edu.au>
2025-06-08 12:44:03 +1000

    Complete answers to the questions in comments for line 1

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+"""
+CP1404 - Practical_03
+Random Numbers Program
+Name: Amie Neill
+"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+# Output is a random integer between 5 and 20
+# Smallest number: 5
+# Largest number: 20
+
+
+
+
+
+
+
