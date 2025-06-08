555e57ebc9f00a159308542efb831d3dae09cd47
amie <amie.neill@my.jcu.edu.au>
2025-06-08 14:52:56 +1000

    Add digit character check in password

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -35,8 +35,9 @@ def is_valid_password(password):
     number_of_digit = 0
     number_of_special = 0
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        if character.isdigit():
+            number_of_digit = number_of_digit + 1
+
 
     # TODO: if any of the 'normal' counts are zero, return False
 
