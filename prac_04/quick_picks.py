db8f38f472052fddbc3f7ed082e122454250132d
amie <amie.neill@my.jcu.edu.au>
2025-06-15 22:56:44 +1000

    Add structure for quick pick program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,23 @@
+"""
+CP1404 - Practical_04
+Name: Amie Neill
+Quick pick program
+"""
+
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+NUMBERS_PER_LINE = 6
+
+number_of_quick_picks = int(input("How many quick picks: "))
+for i in range(number_of_quick_picks):
+    pass
+
+
+
+
+
+
+
+
+
+
