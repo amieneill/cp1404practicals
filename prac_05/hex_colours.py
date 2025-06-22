71fe7a6ad15c5cfb5fc7edc33b0fb4000f3ddba1
amie <amie.neill@my.jcu.edu.au>
2025-06-22 12:38:12 +1000

    Add colour_to_code dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,14 @@
+"""
+CP1404 - Practical_05
+Name: Amie Neill
+Hex Colours
+"""
+
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "AliceBlue": "#f0f8ff",
+                  "Amaranth": "#e52b50", "Amethyst": "#9966cc",
+                  "Baby Pink": "#f4c2c2", "Black Shadows": "#bfafb2",
+                  "Blue Violet": "#8a2be2", "Boysenberry": "873260",
+                  "Burgundy": "#800020", "Cerulean Frost": "#6d9bc3"}
+
+
+
--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -1,7 +1,7 @@
 """
-CP1404/CP5632 Practical
-State names in a dictionary
-File needs reformatting
+CP1404 - Practical_05
+Name: Amie Neill
+State Names
 """
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
