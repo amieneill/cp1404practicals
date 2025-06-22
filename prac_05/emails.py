13f677b200d92d605e02b13601a600ff6747a1c4
amie <amie.neill@my.jcu.edu.au>
2025-06-22 16:29:10 +1000

    Add user response to confirm or change guessed name from email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -13,8 +13,10 @@ def main():
         at_symbol = email.split("@")[0]
         parts = at_symbol.split(".")
         name = " ".join(parts).title()
+        response = input(f"Is your name {name}? (y/n): ")
+        if response != "y" and response != "":
+            name = input("Name: ")
         print(f"Name: {name} ({email})")
         email = input("Email: ")
-
 
 main()
