6b3afc26de8179d404b820fef7770bcb36c04526
amie <amie.neill@my.jcu.edu.au>
2025-06-22 12:57:09 +1000

    Add basic loop to lookup colour code using colour name

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,8 +7,12 @@ Hex Colours
 COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "AliceBlue": "#f0f8ff",
                   "Amaranth": "#e52b50", "Amethyst": "#9966cc",
                   "Baby Pink": "#f4c2c2", "Black Shadows": "#bfafb2",
-                  "Blue Violet": "#8a2be2", "Boysenberry": "873260",
+                  "Blue Violet": "#8a2be2", "Boysenberry": "#873260",
                   "Burgundy": "#800020", "Cerulean Frost": "#6d9bc3"}
 
-
+colour_name = input("Colour name: ")
+while colour_name != "":
+    colour_code = COLOUR_TO_CODE.get(colour_name)
+    print(f"Colour: {colour_name} is {colour_code}")
+    colour_name = input("Colour name: ")
 
