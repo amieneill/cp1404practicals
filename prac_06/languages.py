d32f10959cba17a492fba08ea24ce8437a1bd138
amie <amie.neill@my.jcu.edu.au>
2025-07-14 22:53:12 +1000

    Import ProgrammingLanguage class and add starter code

---
+++ prac_06/languages.py
@@ -0,0 +1,17 @@
+"""
+CP1404 - Practical_06
+Name: Amie Neill
+Language Exercise
+Estimate: 45 minutes
+Actual:
+"""
+
+from programming_language import ProgrammingLanguage
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
