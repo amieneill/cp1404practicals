0bc1f1fd6f3bd8a63e9b029dcafca1f3a0867cd1
amie <amie.neill@my.jcu.edu.au>
2025-07-14 21:10:38 +1000

    Add ProgrammingLanguage class with basic fields

---
+++ prac_06/programming_language.py
@@ -0,0 +1,17 @@
+"""
+CP1404 - Practical_06
+Name: Amie Neill
+Programming Language Exercise
+Estimate: 45 minutes
+Actual:
+"""
+
+class ProgrammingLanguage:
+    """Represents a programming language."""
+    def __init__(self, name, typing, reflection, year=0):
+        """Initialise a programming language instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
