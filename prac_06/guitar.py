cfcd432c00515e2c3edcc87abea0e71553d24ec1
amie <amie.neill@my.jcu.edu.au>
2025-07-15 00:07:36 +1000

    Add is_vintage method and docstrings to Guitar class and methods

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -9,13 +9,21 @@ CURRENT_YEAR = 2025
 
 
 class Guitar:
+    """Represent a Guitar object."""
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return a string representation of the guitar's details."""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
     def get_age(self):
+        """Return the age of the guitar using the current year."""
         return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is vintage (i.e., 50 or more years old)."""
+        return self.get_age() >= 50
