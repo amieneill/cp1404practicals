fe00fdbaa94e15d91d358fc227259b82761e0ef9
amie <amie.neill@my.jcu.edu.au>
2025-07-14 23:11:05 +1000

    Add loop to print dynamically typed languages using is_dynamic method

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -8,10 +8,19 @@ Actual:
 
 from programming_language import ProgrammingLanguage
 
+
 def main():
+    """List and print programming language details."""
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
 
+    languages = [python, ruby, visual_basic]
+    print("The dynamically typed languages are: ")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
 
+
+main()
