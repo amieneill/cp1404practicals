5c5efbf5230fcb879ea012d8d1796a6b9b856ea4
amie <amie.neill@my.jcu.edu.au>
2025-07-15 14:14:29 +1000

    Add code to test get_age() and is_vintage() methods using another_guitar example

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -15,9 +15,12 @@ def main():
     """Run and test Guitar class get_age() and is_vintage() methods."""
 
     guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another_guitar = Guitar("Another Guitar", 2013, 666)
 
     print(f"{guitar.name} get_age() - Expected 103. Got {guitar.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 12. Got {another_guitar.get_age()}")
     print(f"{guitar.name} is_vintage() - Expected True. Got {guitar.is_vintage()}")
+    print(f"{another_guitar.name} is_vintage() - Expected False Got {another_guitar.is_vintage()}")
 
 
 main()
