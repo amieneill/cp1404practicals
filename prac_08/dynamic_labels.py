6c299c30c0b133b4dc681ad8c03fb237dbca33fb
amie <amie.neill@my.jcu.edu.au>
2025-07-30 22:02:59 +1000

    Extract label logic into create_labels function

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -13,15 +13,20 @@ from kivy.uix.label import Label
 
 class DynamicLabelsApp(App):
     """Kivy App that displays dynamic labels for names in a list."""
+
     def build(self):
         """Build the Kivy App GUI using kv file."""
         self.title = 'Dynamic Labels'
         self.root = Builder.load_file('dynamic_labels.kv')
         self.names = ["Archer", "Sia", "Charlie", "Kass", "Willow"]
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        """Create a dynamic label for each name in a list."""
         for name in self.names:
             temp_label = Label(text=name)
             self.root.ids.main.add_widget(temp_label)
-        return self.root
 
 
 DynamicLabelsApp().run()
