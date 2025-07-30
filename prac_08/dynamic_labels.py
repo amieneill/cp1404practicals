2a141b24cdf0dabed28491973dc35728f3aea966
amie <amie.neill@my.jcu.edu.au>
2025-07-30 21:41:42 +1000

    Add initial structure for Dynamic Labels App

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -3,4 +3,21 @@ CP1404 - Practical_08
 Name: Amie Neill
 Dynamic Labels
 Estimate: 2 hours
+Actual:
 """
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class DynamicLabelsApp(App):
+    """Kivy App that displays dynamic labels for names in a list."""
+    def build(self):
+        """Build the Kivy App GUI using kv file."""
+        self.title = 'Dynamic Labels'
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.names = ["Archer", "Sia", "Charlie", "Kass", "Willow"]
+        return self.root
+
+
+DynamicLabelsApp().run()
