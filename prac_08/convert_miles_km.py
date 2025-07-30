4f504718a506ae14465edef44dab59e30e37cbd9
amie <amie.neill@my.jcu.edu.au>
2025-07-30 17:34:23 +1000

    Add miles to kilometres conversion logic and connect to convert button

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -8,15 +8,28 @@ Actual:
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KILOMETRES = 1.60934
 
 
 class MilesToKilometresConverterApp(App):
     """Kivy App that converts miles to kilometres."""
+    km_output = StringProperty()
+
     def build(self):
         """Build the Kivy app GUI using kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
+
+    def handle_conversion(self, text):
+        print('handle conversion')
+        try:
+            miles = float(text)
+            self.km_output = str(miles * MILES_TO_KILOMETRES)
+        except ValueError:
+            self.km_output = "Invalid input"
 
 
 MilesToKilometresConverterApp().run()
