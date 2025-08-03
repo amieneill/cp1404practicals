ca371f2410f52f5b11a47394a0f90124ff922e32
amie <amie.neill@my.jcu.edu.au>
2025-08-03 21:54:32 +1000

    Implement add and play methods to Band class.

--- prac_09/band.py
+++ prac_09/band.py
@@ -15,3 +15,10 @@ class Band:
         """Return a string representation of the Band and musicians."""
         return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
 
+    def add(self, musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
+
+    def play(self):
+        """Return a string of each musician playing an instrument or if they need one."""
+        return "\n".join(musician.play() for musician in self.musicians)
