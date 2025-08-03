b0de6af5df9f0736afb20b2af09aea748dd1e32f
amie <amie.neill@my.jcu.edu.au>
2025-08-03 18:43:52 +1000

    Complete initial taxi tests

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,18 @@
+"""
+CP1404 - Practical_09
+Name: Amie Neill
+Taxi Test
+"""
+from prac_09.taxi import Taxi
+
+
+def main():
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(f"{my_taxi}, Current fare: ${my_taxi.get_fare():.2f}")
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(f"{my_taxi}, Current fare: ${my_taxi.get_fare():.2f}")
+
+
+main()
