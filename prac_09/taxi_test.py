b9a1a9a985c82c46690d1e409a014ea54ed837fd
amie <amie.neill@my.jcu.edu.au>
2025-08-03 18:58:49 +1000

    Complete Taxi exercise

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,7 @@ from prac_09.taxi import Taxi
 
 
 def main():
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(f"{my_taxi}, Current fare: ${my_taxi.get_fare():.2f}")
     my_taxi.start_fare()
