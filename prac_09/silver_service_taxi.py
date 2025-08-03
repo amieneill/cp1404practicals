e2a4f1b57de50b6acd910b48a0ca458ef70f2c32
amie <amie.neill@my.jcu.edu.au>
2025-08-03 21:17:35 +1000

    Add flagfall, __str__, and get_fare methods to SilverServiceTaxi

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -8,8 +8,18 @@ from prac_09.taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Represents a SilverServiceTaxi."""
+    flagfall = 4.5
+    
     def __init__(self, name, fuel, fanciness=0.0):
         """Initialise a SilverServiceTaxi instance."""
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km = self.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string representation of the SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+    
+    def get_fare(self):
+        """Get the fare price."""
+        return self.flagfall + super().get_fare()
