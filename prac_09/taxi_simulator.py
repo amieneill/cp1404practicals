0e91b3603d60974e06638de9ca009c43b7d248bd
amie <amie.neill@my.jcu.edu.au>
2025-08-03 22:21:28 +1000

    Add logic to choose a taxi from list of taxis

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,24 +3,34 @@ CP1404 - Practical_09
 Taxi Simulator Exercise
 Name: Amie Neill
 """
+from prac_09.silver_service_taxi import SilverServiceTaxi
+from prac_09.taxi import Taxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
     """Run the taxi simulator program."""
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    total_bill = 0.0
+
     print("Let's drive!")
     print(MENU)
-    choice = input(">>>").lower()
+    choice = input(">>> ").lower()
     while choice != "q":
-        if choice != "c":
-            pass
+        if choice == "c":
+            print("Taxis available:")
+            for i, taxi in enumerate(taxis):
+                print(f"{i} - {taxi}")
+            taxi_choice = int(input("Choose taxi: "))
         elif choice == "d":
             pass
         else:
             print("Invalid option.")
+        print(f"Bill to date: _____ ")
         print(MENU)
-        choice = input(">>>").lower()
+        choice = input(">>> ").lower()
 
 
 if __name__ == '__main__':
